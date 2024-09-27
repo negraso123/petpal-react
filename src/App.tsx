d381e7fa02e4d920d83f5c2ff251d97345f7b3b1
Sebastian Kennedy <sebastiken@gmail.com>
2024-09-27 11:16:52 -0300

    Initial structure, images, font face

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,40 @@
 import "./App.css";
 
 const logoImage = "logo.png";
+const petpalImage = "petpal.png";
+const huellitas1 = "huellitas1.png";
 
 function App() {
   return (
     <>
-      <video
-        className="background-video absolute top-0 left-0 w-full h-full object-cover -z-10"
-        autoPlay
-        loop
-        muted
-        playsInline
-      >
-        <source src="videos/perro1.mp4" type="video/mp4" />
-        Tu navegador no soporta el elemento de video.
-      </video>
-
-      <audio autoPlay loop>
-        <source src="musica/musica-minecraft.mp3" type="audio/mpeg" />
-        Tu navegador no soporta el elemento de audio.
-      </audio>
-
-      <header className="flex justify-between items-center pt-3 pr-5 bg-gradient-to-b from-transparent to-gray-950 z-10">
-        <div className="logo w-28 h-auto brightness-100">
-          <img src={logoImage} alt="Logo de PetPal" />
-        </div>
-        <div className="links flex gap-4">
-          <a
-            href="mailto:marianomartincervera@itisjt.edu.ar"
-            className="contact-link font-serif text-white font-black hover:underline"
-          >
-            Contacto
-          </a>
-          <a
-            href="https://smallpdf.com/es/file#s=67cb08d4-6598-4c0a-af42-1024f12fe090"
-            className="about-us-link font-serif text-white font-black hover:underline"
-            target="_blank"
-          >
-            Sobre nosotros
-          </a>
+      <header className="bg-marron-900 py-6">
+        <div className="flex justify-end items-center gap-4 px-4">
+          <h1 className="font-titan text-marron-500">Ayuda</h1>
+          <h1 className="font-titan text-marron-500">Contacto</h1>
         </div>
       </header>
-      <div className="container p-20 w-full flex flex-col items-center justify-end font-sans bg-gradient-to-b from-transparent to-gray-950 absolute bottom-0 left-0 z-10">
-        <a href="../register-login/register.html">
-          <button className="register-button p-4 bg-sky-500 max-w-4xl border-none rounded-3xl text-4xl cursor-pointer transition-colors duration-300 ease-in-out text-black mb-5 hover:bg-sky-400 hover:shadow-lg hover:shadow-sky-900">
-            REGISTRATE YA
-          </button>
-        </a>
-        <a
-          href="../register-login/login.html"
-          className="login-link bg-sky-500 font-black text-base mb-4 hover:text-sky-200 hover:underline"
-        >
-          ¿Ya tienes una cuenta? Iniciar Sesión
-        </a>
-      </div>
+      <section className="bg-marron-500 box-border relative">
+        <div className="flex flex-col justify-center items-center">
+          <div className="size-1/12 py-2 mt-10">
+            <img src={logoImage} />
+          </div>
+          <div className="size-3/12 py-2">
+            <img src={petpalImage} />
+          </div>
+          <div className="font-sans text-lg text-marron-900">
+            <p>
+              <span>
+                Tu mascota merece lo mejor,
+                <br />
+              </span>
+              <span>incluso cuando no estás</span>
+            </p>
+          </div>
+          <div className="absolute bottom-0 right-0 w-1/4 h-3/4 invert">
+            <img src={huellitas1} />
+          </div>
+        </div>
+      </section>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-    fontFamily: {
-      sans: ["Super Sunday"],
-      serif: ["Helvetica"],
+    extend: {
+      fontFamily: {
+        titan: ["Titan One"],
+      },
+      colors: {
+        arena: "#eed6a0",
+        "marron-500": "#c29952",
+        "marron-900": "#593e1f",
+      },
     },
   },
   plugins: [],
